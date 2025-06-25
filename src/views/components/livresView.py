3910d57daeb0192c5cc4d0fdd4d07d15bb45404a
Hicham El Mouloudi <hichamelmouloudi04@gmail.com>
2025-06-25 23:53:55 +0100

    - Fix import paths for Livre and LivresModel in their respective files - Linking between the model and view

--- src/views/components/livresView.py
+++ src/views/components/livresView.py
@@ -1,4 +1,7 @@
+# standard library imports
 from tkinter import *
+# custom imports
+from models.livresModel import LivresModel
 
 
 if __name__ == "__main__":
@@ -6,10 +9,16 @@ if __name__ == "__main__":
 
 class LivresView : 
     def __init__(self, parent): 
+        # Linking the model to the view
+        self._model = LivresModel()
+        self._model.loadData() # Load the data from the JSON file 'livres.json'
+        # the container frame for all UI components of this view
         self._frame = Frame(parent)
         self.initUI()
 
     def initUI(self):
+        # TODO: 
+
         return self._frame
 
     def getUI(self):
