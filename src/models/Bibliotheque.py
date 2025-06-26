b2d7fb0da4307d5c9f417be84909d52026ce0a89
Hicham El Mouloudi <hichamelmouloudi04@gmail.com>
2025-06-26 15:44:37 +0100

    Add MembresModel initialization and getter method in Bibliotheque class

--- src/models/Bibliotheque.py
+++ src/models/Bibliotheque.py
@@ -1,5 +1,6 @@
 from src.models.Livre import Livre
 from src.models.livresModel import LivresModel
+from src.models.membresModel import MembresModel
 
 
 if __name__ == "__main__":
@@ -9,7 +10,12 @@ class Bibliotheque :
     def __init__(self) : 
         # Initialize books data
         self._livres = LivresModel()
+        self._membres = MembresModel()
 
     def getLivresModel(self):
         # Return the LivresModel instance
         return self._livres
+
+    def getMembresModel(self):
+        # Return the MembresModel instance
+        return self._membres
