5e1b8ab335453e77889be58d76256af897d40306
Hicham El Mouloudi <hichamelmouloudi04@gmail.com>
2025-06-27 14:51:32 +0100

    Implementing the "Borrowing books section" Ui

--- src/views/components/empruntsView.py
+++ src/views/components/empruntsView.py
@@ -12,11 +12,33 @@ class EmpruntsView:
         self.initUI()
 
     def initUI(self):
-        # Borrowing books section
+        #-------------------------------- Borrowing books section
+        # ------------------------------------------------------------------------------------------
         empruntsFrame = Frame(self._frame, bg="lightblue")
         empruntsFrame.pack(side="left", fill='both', expand=True, padx=10, pady=10)
+        # sub-components for borrowing books
+        empruntsContainerFrame = Frame(empruntsFrame, bg="lightyellow", width=400, height=200)
+        empruntsContainerFrame.pack(pady=10)
+        # labels and entries for borrowing books
+        empruntsIsbnFrame = Frame(empruntsContainerFrame, bg="lightblue")
+        empruntsIsbnFrame.pack(pady=5, fill="both", expand=True)
+        empruntsISBNLabel = Label(empruntsIsbnFrame, text="ISBN du livre à emprunter:", width=26)
+        empruntsISBNLabel.pack(side="left", pady=5, fill="both", expand=True)
+        empruntsISBNEntry = Entry(empruntsIsbnFrame, width=40)
+        empruntsISBNEntry.pack(side="left", pady=5, fill="both", expand=True)
+        #
+        empruntsMemberFrame = Frame(empruntsContainerFrame, bg="lightblue")
+        empruntsMemberFrame.pack(pady=5, fill="both", expand=True)
+        empruntsMembreIdLabel = Label(empruntsMemberFrame, text="ID du membre:", width=26)
+        empruntsMembreIdLabel.pack(side="left", pady=5, fill="both", expand=True)
+        empruntsMembreIdEntry = Entry(empruntsMemberFrame, width=40)
+        empruntsMembreIdEntry.pack(side="left", pady=5, fill="both", expand=True)
+        # button to borrow a book
+        empruntsButton = Button(empruntsFrame, text="Emprunter le livre", command=lambda: self._model.emprunterLivre(empruntsISBNEntry.get(), empruntsMembreIdEntry.get()))
+        empruntsButton.pack(pady=5)
 
-        # Returnning books section
+        # --------------------------------------Returnning books section
+        # ------------------------------------------------------------------------------------------
         retourFrame = Frame(self._frame, bg="lightgreen")
         retourFrame.pack(side="left", fill='both', expand=True, padx=10, pady=10)
         return self._frame
