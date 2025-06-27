c85449b09fb8260298678d98acd85e36340aef65
Hicham El Mouloudi <hichamelmouloudi04@gmail.com>
2025-06-27 13:41:55 +0100

    - Adding basic structure of "EmpruntsModel" and "HistoriqueRecord" - Add LibraryActionEnum and exception classes for handling book borrowing logic

---
+++ src/models/historiqueModel.py
@@ -0,0 +1,56 @@
+
+
+# ----------------------------------------------------------------------------------------
+# ------------------------------------ EmpruntsModel ------------------------------------
+# ----------------------------------------------------------------------------------------
+class LivreIndisponibleError(Exception):
+    def __init__(self, message="Le livre est indisponible pour l'emprunt."):
+        self.message = message
+        super().__init__(self.message)
+# ----------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------------------
+# ------------------------------------ QuotaEmpruntDepasseError ------------------------------------
+# ----------------------------------------------------------------------------------------
+class QuotaEmpruntDepasseError(Exception):
+    def __init__(self, message="Le quota d'emprunt a été dépassé pour ce membre."):
+        self.message = message
+        super().__init__(self.message)
+# ----------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------
+
+
+
+
+
+# ----------------------------------------------------------------------------------------
+# ------------------------------------ EmpruntsModel ------------------------------------
+# ----------------------------------------------------------------------------------------
+class EmpruntsModel:
+    def __init__(self, membresModel, livresModel):
+        self.membresModel = membresModel
+        self.livresModel = livresModel
+        self.emprunts = []
+
+    def toJSON(self, listDesObjHistorique):
+        # transforming a list of HistoriqueRecord objects to a list of dictionaries
+        return [ record.__dict__ for record in listDesObjHistorique ]
+    
+    def fromJSON(self, listDesDictHistorique):
+        # transforming a list of dictionaries to a list of HistoriqueRecord objects
+        return [ HistoriqueRecord(record["unixTime"], record["isbn"], record["membreId"], record["action"]) for record in listDesDictHistorique ]
+
+    def loadData(self):
+        pass
+
+    def saveData(self):
+        pass
+
+    def emprunterLivre(self, isbn, membreId):
+        pass
+
+    def retournerLivre(self, isbn, membreId):
+        pass
