571ea8b109c7a37d60a14a1002c4a1ae13fc4929
Hicham El Mouloudi <hichamelmouloudi04@gmail.com>
2025-06-27 03:36:06 +0100

    Remove commented-out __str__ method and fix formatting in getValuesList method

--- src/models/Membre.py
+++ src/models/Membre.py
@@ -8,14 +8,10 @@ class Membre:
         self.id = id
         self.nom = nom
         self.lesLivresEmpruntes = lesLivresEmpruntes if lesLivresEmpruntes is not None else []
-    
-    # def __str__(self): # This method is used when displaying each member along with the books he has borrowed
-    #     # Return a string representation of the member
-    #     return f"nom : {self.nom}, lesLivresEmpruntes={self.lesLivresEmpruntes})"
 
     def getValuesList(self):
         # Return a list of the member's attributes values
-        return [self.id, self.nom, '\n'.join([ str(livre) for livre in self.lesLivresEmpruntes ])]
+        return [self.id, self.nom, ', '.join([ str(livre) for livre in self.lesLivresEmpruntes ])]
 # ----------------------------------------------------------------------------------------
 # ----------------------------------------------------------------------------------------
 # ----------------------------------------------------------------------------------------
