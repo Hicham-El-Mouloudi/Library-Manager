46a21b7b8ab79a49f9d7f7a1627a67370d31d3fc
Hicham El Mouloudi <hichamelmouloudi04@gmail.com>
2025-06-28 03:19:14 +0100

    Implementing "getHistogrammeData" function

--- src/models/statisticsModel.py
+++ src/models/statisticsModel.py
@@ -1,7 +1,8 @@
 # standard libs
 from collections import Counter # to count the number of repetitions of each element in an array
 # custom libs
-from src.models.livresModel import LivresModel
+from src.models.livresModel import LivresModel, LivreRechercheFiltre
+from src.models.Livre import Livre
 from src.models.membresModel import MembresModel
 from src.models.historiqueModel import HistoriqueModel
 
@@ -26,5 +27,18 @@ class StatisticsModel :
 
     def getHistogrammeData(self) : 
         # getting all history
-        history = self._historiqueModel.getHistoriqueData()
+        historyRecords = self._historiqueModel.getHistoriqueData()
+        # list of all ISBN
+        lesISBN = [historyRecord["ISBN"] for historyRecord in historyRecords.values()]
+        # replace each "ISBN" with its "Author"
+        lesAuteurs = []
+        for isbn in lesISBN : 
+            try : 
+                lesAuteurs.append( self._livresModel.searchLivre(LivreRechercheFiltre.ISBN, isbn).auteur )
+            except Exception as e : 
+                continue # on compte seulemnt les livres valable
+        # Calculating the number of oocurences of each authr and reurn top 10
+        lesAuteursTop10 = Counter(lesAuteurs).most_common(10) # this is a list of tuples
+        
+        return lesAuteursTop10
         
